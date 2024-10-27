392caab5400dcca9d40857d7d6a6c46c5b58c9da
Yuntian Deng <yuntian-deng@users.noreply.huggingface.co>
2024-10-27 10:49:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def predict_next_frame(previous_frames: List[np.ndarray], previous_actions: List
     # Convert the generated frame to the correct format
     new_frame = new_frame.transpose(1, 2, 0)
     print (new_frame.max(), new_frame.min())
-    new_frame_denormalized = denormalize_image(new_frame, source_range=(0, 1))
+    new_frame_denormalized = denormalize_image(new_frame, source_range=(-1, 1))
     
     # Draw the trace of previous actions
     new_frame_with_trace = draw_trace(new_frame_denormalized, previous_actions)
