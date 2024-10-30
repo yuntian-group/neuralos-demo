c5d2e893183eee6b6a0826416cd54a059d95e76e
Yuntian Deng <yuntian-deng@users.noreply.huggingface.co>
2024-10-30 00:00:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def draw_trace(image: np.ndarray, previous_actions: List[Tuple[str, List[int]]])
         x, y = position
         if DEBUG:
             x = x * 256 / 1024
-            y = 6 * 256 / 1024
+            y = y * 256 / 1024
         draw.ellipse([x-2, y-2, x+2, y+2], fill=color)
         
         if i > 0:
