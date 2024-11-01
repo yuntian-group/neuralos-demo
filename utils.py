3f4b6504c5e31c0f02295af037d4e4d049596f1e
Yuntian Deng <yuntian-deng@users.noreply.huggingface.co>
2024-11-01 20:05:24 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -38,15 +38,16 @@ def sample_frame(model: LatentDiffusion, prompt: str, image_sequence: torch.Tens
         c_dict = {'c_crossattn': prompt, 'c_concat': image_sequence}
         c = model.get_learned_conditioning(c_dict)
         c = model.enc_concat_seq(c, c_dict, 'c_concat')
-        
-        samples_ddim, _ = sampler.sample(S=999,
-                                         conditioning=c,
-                                         batch_size=1,
-                                         shape=[3, 64, 64],
-                                         verbose=False, eta=1)
-                                         #unconditional_guidance_scale=5.0,
-                                         #unconditional_conditioning=uc,
-                                         #eta=0)
+
+        samples_ddim = model.p_sample_loop(cond=c, shape=[1, 3, 64, 64], return_intermediates=False, verbose=True)
+        #samples_ddim, _ = sampler.sample(S=999,
+        #                                 conditioning=c,
+        #                                 batch_size=1,
+        #                                 shape=[3, 64, 64],
+        #                                 verbose=False,
+        #                                 unconditional_guidance_scale=5.0,
+        #                                 unconditional_conditioning=uc,
+        #                                 eta=0)
         
         x_samples_ddim = model.decode_first_stage(samples_ddim)
         #x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
