6a0778ce7c18d04fa31a2941917ecfa7b8d4af6f
Yuntian Deng <yuntian-deng@users.noreply.huggingface.co>
2024-11-05 08:23:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def draw_trace(image: np.ndarray, previous_actions: List[Tuple[str, List[int]]],
 
 # Initialize the model at the start of your application
 #model = initialize_model("config_csllm.yaml", "yuntian-deng/computer-model")
-model = initialize_model("2e5_debug_gpt_firstframe_posmap.yaml", "yuntian-deng/computer-model")
+model = initialize_model("2e5_debug_gpt_firstframe_posmap_debugidentity.yaml", "yuntian-deng/computer-model")
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
