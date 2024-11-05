300bdd5f0ebf29a99e2aea465356f0c7e27791a5
Yuntian Deng <yuntian-deng@users.noreply.huggingface.co>
2024-11-05 05:17:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def draw_trace(image: np.ndarray, previous_actions: List[Tuple[str, List[int]]],
         if DEBUG:
             x = x * 256 / 1024
             y = y * 256 / 640
-        draw.ellipse([x-2, y-2, x+2, y+2], fill=color)
+        #draw.ellipse([x-2, y-2, x+2, y+2], fill=color)
         
         
         if prev_x is not None:
