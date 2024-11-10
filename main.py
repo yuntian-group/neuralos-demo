61e3a53626bb7a54eeef6f3cbc832376972ef462
Yuntian Deng <yuntian-deng@users.noreply.huggingface.co>
2024-11-10 08:07:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -97,9 +97,9 @@ def draw_trace(image: np.ndarray, previous_actions: List[Tuple[str, List[int]]],
         #draw.ellipse([x-2, y-2, x+2, y+2], fill=color)
         
         
-        if prev_x is not None:
-            #prev_x, prev_y = previous_actions[i-1][1]
-            draw.line([prev_x, prev_y, x, y], fill=color, width=1)
+        #if prev_x is not None:
+        #    #prev_x, prev_y = previous_actions[i-1][1]
+        #    draw.line([prev_x, prev_y, x, y], fill=color, width=1)
         prev_x, prev_y = x, y
     draw.ellipse([x_scaled*4-2, y_scaled*4-2, x_scaled*4+2, y_scaled*4+2], fill=(0, 255, 0))
     #pil_image = pil_image.convert("RGB")
