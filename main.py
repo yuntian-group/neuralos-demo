3b110059f1809819b390ce40a542267cf7f3874e
da03 <dengyuntian@gmail.com>
2024-12-07 18:45:04 -0500

    .

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ def predict_next_frame(previous_frames: List[np.ndarray], previous_actions: List
         prompt = 'N + 0 0 1 6 : + 0 3 2 8 N + 0 3 0 4 : + 0 0 9 6 N + 0 2 4 0 : + 0 1 9 2 N + 0 1 5 2 : + 0 0 5 6 L + 0 2 8 8 : + 0 1 7 6 L + 0 0 5 6 : + 0 3 7 6 N + 0 1 3 6 : + 0 3 6 0 N + 0 1 1 2 : + 0 0 4 8'
         previous_actions = [('move', (16, 328)), ('move', (304, 96)), ('move', (240, 192)), ('move', (152, 56)), ('left_click', (288, 176)), ('left_click', (56, 376)), ('move', (136, 360)), ('move', (112, 48))]
         prompt = 'L + 0 0 5 6 : + 0 1 2 8 N + 0 4 0 0 : + 0 0 6 4 N + 0 5 0 4 : + 0 1 2 8 N + 0 4 2 4 : + 0 1 2 0 N + 0 3 2 0 : + 0 1 0 4 N + 0 2 8 0 : + 0 1 0 4 N + 0 2 7 2 : + 0 1 0 4 N + 0 2 7 2 : + 0 1 0 4'
-        previous_actions = [('left_click', (56, 128)), ('left_click', (400, 64)), ('move', (504, 128)), ('move', (424, 120)), ('move', (320, 104)), ('move', (280, 104)), ('move', (272, 104)), ('move', (272, 104))]
+        previous_actions = [('left_click', (56, 128)), ('left_click', (400, 64)), ('move', (504, 128)), ('move', (424, 120)), ('move', (320, 104)), ('left_click', (280, 104)), ('move', (272, 104)), ('move', (272, 104))]
     for action_type, pos in previous_actions: #[-8:]:
         #print ('here3', action_type, pos)
         if action_type == 'move':
