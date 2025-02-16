60103b50cf39840b9d8fc95f73cf101913c05706
da03 <dengyuntian@gmail.com>
2025-02-15 21:09:05 -0500

    .

--- utils.py
+++ utils.py
@@ -82,7 +82,7 @@ def sample_frame(model: LatentDiffusion, prompt: str, image_sequence: torch.Tens
         if DDPM:
             samples_ddim = model.p_sample_loop(cond=c, shape=[1, 4, 48, 64], return_intermediates=False, verbose=True)
         else:
-            samples_ddim, _ = sampler.sample(S=4,
+            samples_ddim, _ = sampler.sample(S=8,
                                          conditioning=c,
                                          batch_size=1,
                                          shape=[4, 48, 64],
