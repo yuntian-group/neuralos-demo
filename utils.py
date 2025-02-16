087bec258e0644e5504185b08379e8b63458b7d4
da03 <dengyuntian@gmail.com>
2025-02-16 13:56:58 -0500

    .

--- utils.py
+++ utils.py
@@ -44,7 +44,7 @@ def sample_frame(model: LatentDiffusion, prompt: str, image_sequence: torch.Tens
         #print (c['c_crossattn'].shape)
         #print (c['c_crossattn'][0])
         print (prompt)
-        c = {'c_concat': image_sequence}
+        c = {'c_concat': image_sequence.unsqueeze(0)}
         #c = model.enc_concat_seq(c, c_dict, 'c_concat')
         # Zero out the corresponding subtensors in c_concat for padding images
         #padding_mask = torch.isclose(image_sequence, torch.tensor(-1.0), rtol=1e-5, atol=1e-5).all(dim=(1, 2, 3)).unsqueeze(0)
