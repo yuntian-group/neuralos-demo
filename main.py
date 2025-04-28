0927fc2f3e56a834936b23369f9a76e16359204a
da03 <dengyuntian@gmail.com>
2025-04-27 21:18:26 -0400

    .

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ NUM_MAX_FRAMES = 2
 
 SCREEN_WIDTH = 512
 SCREEN_HEIGHT = 384
-NUM_SAMPLING_STEPS = 64
+NUM_SAMPLING_STEPS = 32
 
 with open('latent_stats.json', 'r') as f:
     latent_stats = json.load(f)
